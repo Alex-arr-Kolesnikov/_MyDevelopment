57badc9bf3bfb547a3204e25ef23f1df5ee6c494
Alex-arr-Kolesnikov <aleksei0734684589@gmail.com>
2019-11-29 11:55:07 +0200

    add in libs file parallax and section features

--- Автоматизированная_штукатурка_стен/src/index.js
+++ Автоматизированная_штукатурка_стен/src/index.js
@@ -1,2 +1,4 @@
+
 import "./js/common.js";
+import "./assets/libs/parallax/parallax.min.js";
 import "./assets/sass/main.sass";
--- Автоматизированная_штукатурка_стен/src/js/common.js
+++ Автоматизированная_штукатурка_стен/src/js/common.js
@@ -8,5 +8,24 @@ $(document).ready(function () {
         $('.popup').hide();
     });
 
+    $('.features-slider').owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        smartSpeed: 700,
+        navText: [
+            '<i class="fa fa-angle-left"></i>',
+            '<i class="fa fa-angle-right"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            992: {
+                items: 4
+            }
+        }
+    });
 
 });
