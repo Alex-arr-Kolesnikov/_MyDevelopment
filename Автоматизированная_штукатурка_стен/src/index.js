33110365f0aa5a2698c0249432df713d83b2ff48
Alex-arr-Kolesnikov <aleksei0734684589@gmail.com>
2019-12-04 09:00:19 +0200

    add fotorama and section feedback

--- Автоматизированная_штукатурка_стен/src/index.js
+++ Автоматизированная_штукатурка_стен/src/index.js
@@ -1,4 +1,5 @@
 
 import "./js/common.js";
 import "./assets/libs/parallax/parallax.min.js";
+import "./assets/libs/fotorama/fotorama.js";
 import "./assets/sass/main.sass";
--- Автоматизированная_штукатурка_стен/src/js/common.js
+++ Автоматизированная_штукатурка_стен/src/js/common.js
@@ -28,4 +28,15 @@ $(document).ready(function () {
         }
     });
 
+    $('.feedback-slider').owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+        dots: false,
+        smartSpeed: 700,
+        navText: [
+            '<i class="fa fa-angle-left"></i>',
+            '<i class="fa fa-angle-right"></i>'
+        ]
+    });
 });
