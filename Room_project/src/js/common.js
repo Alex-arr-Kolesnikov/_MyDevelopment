052f82bf31c9f1ecb0ceeade1ccd352e6487ac76
Alex-arr-Kolesnikov <aleksei0734684589@gmail.com>
2019-12-18 20:37:03 +0200

    add section steps and services

--- Room_project/src/js/common.js
+++ Room_project/src/js/common.js
@@ -4,12 +4,13 @@ $(() => {
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../assets/img/apartments/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../assets/img/apartments/arrow-right.svg" alt=""></button>',
         responsive: [
             {
                 breakpoint: 1200,
                 settings: {
                     slidesToShow: 2,
-                    prevArrow: '<button class="prev"></button>',
                     slidesToScroll: 2
                 }
             },
@@ -30,3 +31,28 @@ $(() => {
         ]
     });
 });
+
+let stepBlock1 = document.getElementById("stepBlock1");
+let stepBorder1 = document.getElementById("stepBorder1");
+let stepBlock2 = document.getElementById("stepBlock2");
+let stepBorder2 = document.getElementById("stepBorder2");
+let stepBlock3 = document.getElementById("stepBlock3");
+let stepBorder3 = document.getElementById("stepBorder3");
+stepBlock1.onmouseover = () => {
+    stepBorder1.style.borderBottom = "3px solid white";
+};
+stepBlock1.onmouseout = () => {
+    stepBorder1.style.borderBottom = "3px solid black";
+};
+stepBlock2.onmouseover = () => {
+    stepBorder2.style.borderBottom = "3px solid white";
+};
+stepBlock2.onmouseout = () => {
+    stepBorder2.style.borderBottom = "3px solid black";
+};
+stepBlock3.onmouseover = () => {
+    stepBorder3.style.borderBottom = "3px solid white";
+};
+stepBlock3.onmouseout = () => {
+    stepBorder3.style.borderBottom = "3px solid black";
+};
