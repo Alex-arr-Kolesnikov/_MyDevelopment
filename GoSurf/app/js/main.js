51b835bce8f4c4b301ea5ebe7ee44a70b0a05971
Alex-arr-Kolesnikov <aleksei0734684589@gmail.com>
2020-01-08 16:55:03 +0200

    custom section Surf

--- GoSurf/app/js/main.js
+++ GoSurf/app/js/main.js
@@ -14,5 +14,12 @@ $(() => {
         asNavFor: '.header__slider'
     });
 
+    $('.surf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/header/arrows-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/header/arrows-right.svg" alt="">'
+    });
+
 });
 
