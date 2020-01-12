e2310e6b3733c9241aef1523d49b6918e0fe2861
Alex-arr-Kolesnikov <aleksei0734684589@gmail.com>
2020-01-12 19:13:00 +0200

    create surf-map

--- GoSurf/app/js/main.js
+++ GoSurf/app/js/main.js
@@ -18,7 +18,16 @@ $(() => {
         slidesToShow: 4,
         slidesToScroll: 1,
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/header/arrows-left.svg" alt="">',
-        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/header/arrows-right.svg" alt="">'
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/header/arrows-right.svg" alt="">',
+        asNavFor: '.surf-map'
+    });
+
+    $('.surf-map').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.surf-slider',
+        focusOnSelect: true
     });
 
 });
