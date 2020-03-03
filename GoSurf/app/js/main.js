a9b3f6f48bfcfe1a436ce5aba487771d0511bca7
Alex-arr-Kolesnikov <aleksei0734684589@gmail.com>
2020-03-03 15:35:30 +0200

    create section travel

--- GoSurf/app/js/main.js
+++ GoSurf/app/js/main.js
@@ -30,5 +30,11 @@ $(() => {
         focusOnSelect: true
     });
 
-});
+    $('.travel__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/header/arrows-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/header/arrows-right.svg" alt="">'
+    });
 
+});
