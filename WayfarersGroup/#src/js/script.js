821ee850593692eb6748f9b4fb080cb18fce8d94
Alex lenevo <aleksei0734684589@gmail.com>
2020-05-23 22:35:23 +0300

    add slider reviews

---
+++ WayfarersGroup/#src/js/script.js
@@ -0,0 +1,14 @@
+@@include('./slick.min.js')
+
+
+$(document).ready(function(){
+	$('.slider__box').slick({
+		arrows:true,
+		dots:true,
+		slidesToShow:1,
+		autoplay:true,
+		speed:1000,
+		autoplaySpeed:800
+	});
+});
+
