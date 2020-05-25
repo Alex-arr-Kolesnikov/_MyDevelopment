99374761191563468af5aececee5b13b2692cd16
Alex lenevo <aleksei0734684589@gmail.com>
2020-05-25 20:08:41 +0300

    add section info

--- WayfarersGroup/WayfarersGroup/js/script.js
+++ WayfarersGroup/WayfarersGroup/js/script.js
@@ -11,4 +11,14 @@ $(document).ready(function () {
 		speed: 1000,
 		autoplaySpeed: 5000
 	});
+
+	$('.slider__agents').slick({
+		arrows: true,
+		slidesToShow: 3,
+		autoplay: true,
+		speed: 1000,
+		autoplaySpeed: 5000,
+		prevArrow: '<button type="button" class="slick-prev">&#8668</button>',
+		nextArrow: '<button type="button" class="slick-next">&#8669</button>'
+	});
 });
