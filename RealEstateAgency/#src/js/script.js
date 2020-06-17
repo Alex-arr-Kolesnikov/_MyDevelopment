e1b4728e176b7ccc5df17c254ad00e42656f747d
Alex lenevo <aleksei0734684589@gmail.com>
2020-06-17 15:52:59 +0300

    add, agents, footer

--- RealEstateAgency/#src/js/script.js
+++ RealEstateAgency/#src/js/script.js
@@ -1,11 +1,9 @@
-@@include('./slick.min.js')
-
 $(document).ready(function () {
     $('.apartments__slider').slick({
         arrows: true,
         infinite: true,
 		slidesToShow: 3,
-		//autoplay: true,
+		autoplay: true,
 		speed: 1000,
         autoplaySpeed: 5000,
         prevArrow: '<button type="button" class="slick-prev"><img src="./img/apartments/arrow-left.svg" alt="button"></button>',
@@ -35,3 +33,5 @@ $(document).ready(function () {
         ]
     });
 });
+
+@@include('./slick.min.js')
