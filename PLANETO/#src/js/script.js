9b41657bcb7399ee0641fb5a69fff0456d4fc552
Alex lenevo <aleksei0734684589@gmail.com>
2020-07-02 12:06:39 +0300

    предварительный конец разработки

--- PLANETO/#src/js/script.js
+++ PLANETO/#src/js/script.js
@@ -3,18 +3,18 @@
 
 $(document).ready(function () {
 
-    $('.reviews__slider').slick({
+    $('.partners__slider').slick({
         arrows: true,
         slidesToShow: 3,
         autoplay: true,
         speed: 1000,
         autoplaySpeed: 5000,
-        prevArrow: '<button type="button" class="slick-prev"><img src="./img/reviews/prevArrow.svg" alt="Arrow"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="./img/reviews/nextArrow.svg" alt="Arrow"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="./img/prevArrow.svg" alt="Arrow"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="./img/nextArrow.svg" alt="Arrow"></button>',
         responsive: [{
-            breakpoint: 1440,
+            breakpoint: 1200,
             settings: {
-                slidesToShow: 1
+                slidesToShow: 4
             }
         }]
     });
