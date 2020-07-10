062d2b1e98bcf4dcf899d06116beeb6982dfd16c
Alex lenevo <aleksei0734684589@gmail.com>
2020-07-10 10:10:29 +0300

    end project 1.0

--- МечтаМастера/src/js/script.js
+++ МечтаМастера/src/js/script.js
@@ -1,5 +1,23 @@
+@@include('./slick.min.js')
+
 let header_phone = document.querySelector('.header__actions-phone');
 header_phone.addEventListener("click", function (e) {
     let header_tel = document.querySelector('.header__actions-tel');
     header_tel.classList.toggle('_active');
 });
+
+$(document).ready(function () {
+
+    $('.feedback__box').slick({
+        arrows: true,
+		dots: true,
+		slidesToShow: 1,
+		autoplay: true,
+		speed: 1000,
+        autoplaySpeed: 5000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="./img/prevArrow.svg" alt="button"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="./img/nextArrow.svg" alt="button"></button>'
+    });
+
+});
+
