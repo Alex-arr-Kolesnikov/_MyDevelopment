f520ad3eada06fa3dc67340c9733452ad1c41ab3
Alex lenevo <aleksei0734684589@gmail.com>
2020-07-12 12:44:42 +0300

    finish 1.1

--- МечтаМастера/src/js/script.js
+++ МечтаМастера/src/js/script.js
@@ -1,10 +1,28 @@
-@@include('./slick.min.js')
+@@include('./slick.min.js');
 
 let header_phone = document.querySelector('.header__actions-phone');
 header_phone.addEventListener("click", function (e) {
     let header_tel = document.querySelector('.header__actions-tel');
     header_tel.classList.toggle('_active');
 });
+
+let popap_btn = document.querySelector('.price__box-btn');
+popap_btn.addEventListener("click", function (e) {
+    let popap_form = document.querySelector('.price__popap');
+    popap_form.classList.toggle('_active');
+});
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+for (let anchor of anchors) {
+    anchor.addEventListener("click", function(event) {
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href')
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    });
+};
 
 $(document).ready(function () {
 
@@ -18,6 +36,4 @@ $(document).ready(function () {
         prevArrow: '<button type="button" class="slick-prev"><img src="./img/prevArrow.svg" alt="button"></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="./img/nextArrow.svg" alt="button"></button>'
     });
-
 });
-
