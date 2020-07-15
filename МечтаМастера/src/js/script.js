62bd179ecb80d6648109127b41242a720fb8c0e5
Alex lenevo <aleksei0734684589@gmail.com>
2020-07-15 15:09:50 +0300

    Дополнения и изменения popup

--- МечтаМастера/src/js/script.js
+++ МечтаМастера/src/js/script.js
@@ -6,6 +6,21 @@ header_phone.addEventListener("click", function (e) {
     header_tel.classList.toggle('_active');
 });
 
+$(document).ready(function () {
+
+    $('.feedback__box').slick({
+        arrows: true,
+		dots: true,
+		slidesToShow: 1,
+		autoplay: true,
+		speed: 1000,
+        autoplaySpeed: 5000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="./img/prevArrow.svg" alt="button"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="./img/nextArrow.svg" alt="button"></button>'
+    });
+});
+
+/*
 let popap_btn = document.querySelector('.price__box-btn');
 popap_btn.addEventListener("click", function (e) {
     let popap_form = document.querySelector('.price__popap');
@@ -23,17 +38,4 @@ for (let anchor of anchors) {
         });
     });
 };
-
-$(document).ready(function () {
-
-    $('.feedback__box').slick({
-        arrows: true,
-		dots: true,
-		slidesToShow: 1,
-		autoplay: true,
-		speed: 1000,
-        autoplaySpeed: 5000,
-        prevArrow: '<button type="button" class="slick-prev"><img src="./img/prevArrow.svg" alt="button"></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="./img/nextArrow.svg" alt="button"></button>'
-    });
-});
+*/
